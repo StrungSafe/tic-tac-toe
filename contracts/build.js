8724f64b0c4ef65c3b753cef0440fcf3260ac964
Miguel Molina Jr. <molim134@gmail.com>
2025-07-01 12:08:43 -0400

    Working on automated tests

--- contracts/build.js
+++ contracts/build.js
@@ -2,6 +2,8 @@ import { compileFile } from 'cashc';
 import { writeFile } from 'fs';
 
 const gameArtifact = compileFile(new URL('game.cash', import.meta.url));
-writeFile('artifacts/game.cash.artifact', stringify(gameArtifact), error => {
-    console.error('There was an error writing the game artifact to disk', error);
+writeFile('artifacts/game.cash.artifact', JSON.stringify(gameArtifact), error => {
+    if(error) {
+        throw error;
+    }
 });
